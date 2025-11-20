80112322c0e049d650dbad3821b326994b7b67cc
bhy811@myy.haaga-helia.fi <bhy811@myy.haaga-helia.fi>
2025-11-20 16:48:03 +0200

    P2

--- javascript.js
+++ javascript.js
@@ -27,7 +27,7 @@ const kuvat = [
 const date = new Date();
 const currentMonth = date.getMonth() + 1;
 const currentDay = date.getDate();
-createDivs();
+createDivs(24, ".calendarGrid");
 
 function isCorrectDate(day){
     if (currentMonth == 12 && currentDay >= day){
@@ -40,22 +40,24 @@ function isCorrectDate(day){
 
 function showPicture(day){
     if (isCorrectDate(day)){
+       let divID = document.getElementById("day" + day);
+       divID.style.backgroundImage = "url(" + kuvat[day - 1] + ")";
+       divID.innerText = "";
        
-       
-        
     }
     else{
         
     }
 }
 
-function createDivs(){
-    const calendarGrid = document.querySelector(".calendarGrid");
+function createDivs(amount, location){
+    const calendarGrid = document.querySelector(location);
     
-    for (let i = 1; i <= 24; i++){
+    for (let i = 1; i <= amount; i++){
         const newDiv = document.createElement("div");
         newDiv.id = "day" + i;
         newDiv.innerText = i;
+        newDiv.addEventListener("click", function(){showPicture(i);});
         calendarGrid.appendChild(newDiv);
     }
 }
