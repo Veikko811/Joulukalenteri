ad471f9890da3be4f49334db4569f37abb6d9cd2
kallehonkanenn <bhq723@myy.haaga-helia.fi>
2025-11-21 16:42:01 +0200

    Error effekti

--- javascript.js
+++ javascript.js
@@ -3,34 +3,46 @@ const currentMonth = date.getMonth() + 1;
 const currentDay = date.getDate();
 createDivs(24, ".calendarGrid");
 
-function isCorrectDate(day){
-    if (currentMonth == 12 && currentDay >= day){
+function isCorrectDate(day) {
+    if (currentMonth == 12 && currentDay >= day) {
         return true;
     }
-    else{
+    else {
         return false;
     }
 }
 
-function showPicture(day){
-    if (isCorrectDate(day)){
-       let divID = document.getElementById("day" + day);
-       divID.style.backgroundImage = "url()";
-       divID.innerText = "";
+function showPicture(day) {
+    if (isCorrectDate(day)) {
+        let divID = document.getElementById("day" + day);
+        divID.style.backgroundImage = "url()";
+        divID.innerText = "";
     }
-    else{
-        
+    else {
+        divID.classList.add("shake");
+
+        // Poistetaan animaatioluokka hetken kuluttua, jotta se voi toistua
+        setTimeout(() => {
+            divID.classList.remove("shake");
+        }, 500);
+
+        // Soitetaan ääniefekti
+        const audio = new Audio("Kuvat/error.mp3"); // Lisää tiedosto kansioon
+        audio.play();
+
+
+
     }
 }
 
-function createDivs(amount, location){
+function createDivs(amount, location) {
     let calendarGrid = document.querySelector(location);
 
-    for (let i = 1; i <= amount; i++){
+    for (let i = 1; i <= amount; i++) {
         let newDiv = document.createElement("div");
         newDiv.id = "day" + i;
         newDiv.innerText = i;
-        newDiv.addEventListener("click", function(){showPicture(i);});
+        newDiv.addEventListener("click", function () { showPicture(i); });
         calendarGrid.appendChild(newDiv);
     }
 }
