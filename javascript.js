0fe5db29642d3219ff5dcc15c31c064f436bb42a
bhy811@myy.haaga-helia.fi <bhy811@myy.haaga-helia.fi>
2025-11-22 23:06:31 +0200

    P5

--- javascript.js
+++ javascript.js
@@ -1,9 +1,10 @@
 const date = new Date();
 const currentMonth = date.getMonth() + 1;
 const currentDay = date.getDate();
+const audio = new Audio("Kuvat/error.mp3");
 
-function shuffledNumbers(n) {
-    const arr = Array.from({ length: n }, (_, i) => i + 1);
+function shuffledNumbers() {
+    const arr = Array.from({ length: 24 }, (_, i) => i + 1);
     for (let i = arr.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
         [arr[i], arr[j]] = [arr[j], arr[i]];
@@ -13,48 +14,29 @@ function shuffledNumbers(n) {
 
 const numbers = shuffledNumbers(24);
 
-function isCorrectDate(day) {
-    if (currentMonth == 12 && currentDay >= day) {
-        return true;
-    }
-    else {
-        return false;
-    }
+function removeTopImage() {
+    this.style.backgroundImage = "url()";
+    this.innerText = "";
+    this.removeEventListener("click", removeTopImage);
+}
+function playShakeAnimation(){
+    this.style.animation = "shake 0.5s";
+    setTimeout(() => {this.style.animation = "";}, 500);
+    audio.play();
 }
 
-function showPicture(day) {
-    if (isCorrectDate(day)) {
-        let divID = document.getElementById("day" + day);
-        divID.style.backgroundImage = "url()";
-        divID.innerText = "";
+const calendarGrid = document.querySelector(".calendarGrid");
+
+for (let i = 1; i <= 24; i++) {
+    let num = numbers[i - 1]; // shuffled number
+    let newDiv = document.createElement("div");
+    newDiv.innerText = num;   // näyttää shuffled number
+    if (currentMonth == 11 && currentDay >= num){
+        newDiv.addEventListener("click", removeTopImage);
     }
-    else {
-        let divID = document.getElementById("day" + day);
-        divID.classList.add("shake");
-
-        // Poistetaan animaatioluokka kun animaatio loppuu
-        divID.addEventListener("animationend", () => {
-            divID.classList.remove("shake");
-        }, { once: true });
-
-        // Soitetaan ääniefekti
-        const audio = new Audio("Kuvat/error.mp3");
-        audio.play();
-    }
-
+    else{
+        newDiv.addEventListener("click", playShakeAnimation)
+    };
+    calendarGrid.appendChild(newDiv);
 }
 
-function createDivs(amount, location) {
-    let calendarGrid = document.querySelector(location);
-
-    for (let i = 0; i < amount; i++) {
-     let num = numbers[i]; // shuffled number
-
-        let newDiv = document.createElement("div");
-        newDiv.id = "day" + num;  // ID käyttää shuffled number
-        newDiv.innerText = num;   // näyttää shuffled number
-        newDiv.addEventListener("click", () => showPicture(num));
-        calendarGrid.appendChild(newDiv);
-    }
-}
-createDivs(24, ".calendarGrid");
